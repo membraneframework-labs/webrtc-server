c632fd78610788a5324414dffb09d7641132bd7b
DominikWolek <ddww12@gmail.com>
2019-07-23 17:00:35 +0200

    Update application to use DynamicSupervisor

--- lib/webrtc_server.ex
+++ lib/webrtc_server.ex
@@ -1,16 +1,14 @@
 defmodule Membrane.WebRTC.Server do
   @moduledoc false
   use Application
+  use DynamicSupervisor
 
   def start(_type, _args) do
-    children = [
-      Registry.child_spec(
-        keys: :duplicate,
-        name: Server.Registry
-      )
-    ]
-
-    opts = [strategy: :one_for_one, name: __MODULE__]
-    Supervisor.start_link(children, opts)
+    {:ok, _} = DynamicSupervisor.start_link(__MODULE__, :ok, name: __MODULE__)
+    registry = Registry.child_spec(keys: :duplicate, name: Server.Registry)
+    {:ok, _} = DynamicSupervisor.start_child(Membrane.WebRTC.Server, registry)
   end
+
+  def init(_),
+    do: {:ok, %{strategy: :one_for_one}}
 end
