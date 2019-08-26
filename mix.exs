794cdac59ba747011b7146f1eb6d70563767f3da
DominikWolek <ddww12@gmail.com>
2019-08-26 14:27:12 +0200

    Add more verbose README
    Alsoo bumped version from 0.1 to 1.0.

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Membrane.WebRTC.Server.MixProject do
   use Mix.Project
 
-  @version "0.1.0"
+  @version "1.0.0"
   @github_url "https://github.com/membraneframework/webrtc-server"
 
   def project do
